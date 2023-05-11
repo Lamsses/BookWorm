93815e2ad1fad6748304f23c70a1896d9f1725e5
mohaymen <mohaymen@gmail.com>
2023-05-11 07:34:10 +0300

    s

---
+++ app/src/main/java/com/example/myapplication/ForYouAll.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForYouAll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.for_you_expanded);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
+     TextView SeeAll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         SeeAll = findViewById(R.id.seeall_foryou_text);
+        SeeAll.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this,ForYouAll.class)));
+
 
     }
 }
